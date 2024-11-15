3fe8ef66f565b89443c3877dee99ae22ef102512
LBQTrung <lebaquoctrung@gmail.com>
2024-11-15 09:41:29 +0700

    2. Add llm model to generate report in pdf format

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
-from fastapi import FastAPI, UploadFile, File, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
-import whisper
-from typing import List
-import asyncio
-import torch
-from tqdm import tqdm
 import os
 import uuid
+from models.whisper_model import transcribe_audio
+from models.llm_model import report_generator, create_pdf_from_html
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="templates"), name="static")
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using device: {device if device != 'cuda' else torch.cuda.get_device_name(0)}")
-model = whisper.load_model("small", device=device)
 
 # Serve the HTML interface
 @app.get("/", response_class=HTMLResponse)
@@ -25,31 +18,24 @@ async def get_html():
     return HTMLResponse(content=html_content, status_code=200)
 
 # Transcription route
-@app.post("/transcribe/")
-async def transcribe_audios(files: List[UploadFile] = File(...)):
-    transcriptions = []
+@app.post("/report-generate")
+async def transcribe_audios(file: UploadFile = File(...)):
     unique_code = uuid.uuid4()
-    file_index = 0
-
     os.makedirs("audios", exist_ok=True)
     os.makedirs("texts", exist_ok=True)
 
-    async def process_file(file: UploadFile):
-        audio_data = await file.read()
-        unique_filename = f"audios/{unique_code}_{file_index}.mp3"
-        with open(unique_filename, "wb") as f:
-            f.write(audio_data)
-        result = model.transcribe(unique_filename)
-        transcriptions.append({"filename": file.filename, "text": result["text"]})
-        
-        transcription_filename = f"texts/{unique_code}_{file_index}.txt"
-        with open(transcription_filename, "w", encoding="utf-8") as f:
-            f.write(result["text"])
+    file_name = file.filename.split(".")[0]
+    audio_data = await file.read()
+    unique_filename = f"audios/{unique_code}_{file_name}.mp3"
+    with open(unique_filename, "wb") as f:
+        f.write(audio_data)
 
-        # os.remove(unique_filename)  # Delete the file after transcription
-
-    for file in tqdm(files, desc="Transcribing files"):
-        await process_file(file)
-        file_index += 1
-
-    return {"transcriptions": transcriptions}
+    text_result = await transcribe_audio(unique_filename)
+    transcription_filename = f"texts/{unique_code}_{file_name}.txt"
+    with open(transcription_filename, "w", encoding="utf-8") as f:
+        f.write(text_result) 
+    # os.remove(unique_filename)
+    
+    report_html = await report_generator(transcription_filename)
+    report_pdf = create_pdf_from_html(report_html)
+    return StreamingResponse(report_pdf, media_type="application/pdf", headers={"Content-Disposition": "inline; filename=report.pdf"})
